e395aa00b17662b837fc0aafd9fe5e9e2c77e319
Pedro Rocha <pedryvo@gmail.com>
2026-03-17 14:16:27 -0300

    fix(db): update seed script to use unpooled neon connection

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client";
 import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-const connectionString = process.env.DATABASE_URL;
+const connectionString = process.env.NEON_DATABASE_URL;
 const pool = new Pool({ connectionString });
 const adapter = new PrismaPg(pool as any);
 const prisma = new PrismaClient({ adapter });
