375022a4dc95738267de39ba9bd3bcac541126c6
Pedro Rocha <pedryvo@gmail.com>
2026-03-17 14:16:27 -0300

    chore: add automatic db seed to postinstall script

--- app/mensagens/page.tsx
+++ app/mensagens/page.tsx
@@ -14,6 +14,7 @@ import {
 import { Card, CardContent } from "@/components/ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
